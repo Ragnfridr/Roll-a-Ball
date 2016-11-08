f19d0bd7416449b5d07c90fd4d5d996640225157
Cecilia S <Ragnfridr@gmail.com>
2016-11-08 12:17:36 +0100

    Added basic working program
    A rolling ball and pick ups on a plane, player controls and a scoring
    system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed = 100;
+    private Rigidbody rb;
+
+    public Text countText;
+    public Text winText;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCounterText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        // hantera fysik efter input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCounterText();
+        }
+    }
+
+    void SetCounterText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "Congratulations! You win!";
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+    public int rotateSpeed = 3;
+	
+	void Update () {
+        // deltaTime; time since game started ~ frames / second
+        // in this way we aren't dependent on the processor speed
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * rotateSpeed);
+	}
+}
